26843fd99d7dd69a4115ebe798bbcab04c50db25
Debora Silva <deborafllyon@gmail.com>
2023-07-02 13:00:50 -0300

    Exclui e recriei novamente a classe AccountEntity

---
+++ src/test/java/entities/AccountEntity.java
@@ -0,0 +1,7 @@
+package entities;
+// CRIAMOS OS CAMPOS QUE EU QUERO NA MINHA ENTIDADE
+public class AccountEntity { // Entidade que representa o usuário no sistema
+    public String userName; // Usuário
+    public String password; // Senha.
+
+}
