d88e20c8c7a6f2764920f1db85b63d85bd45c72e
Debora Silva <deborafllyon@gmail.com>
2023-07-15 15:56:20 -0300

    Completo o script simples com Selenium e criamos um arquivo de feature para a mesma funcionalidade

--- src/test/java/webtest/seleniumSimples.java
+++ src/test/java/webtest/seleniumSimples.java
@@ -3,13 +3,20 @@ package webtest;
 // 2 - Bibliotecas
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 // 3 - Classe
 public class seleniumSimples {
@@ -36,13 +43,59 @@ public class seleniumSimples {
        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
        driver.manage().window().maximize();//maximiza a janela do navegador
     }
-
-    // 3.4 Depois dp Teste
+    // 3.4 Depois do Teste
     @AfterMethod
     public void tearDown() {
         driver.close(); //destroi o objeto do Selenium WebDriver
     }
 
     // 3.5 Teste em si
+    @Test
+public void TestarSelectBackdrop(){
+        //Abrir a pagina inicial do site SauceDemo
+        driver.get("https://www.saucedemo.com");
 
+        //Digitar o usuario e a senha
+        // Clicar no elemento antes dfe escrever
+        WebElement username = driver.findElement(By.id("user-name"));// controla a caixa de texto
+        username.click();// clicar na caixa de texto
+        username.clear();//limpa caixa de texto
+        username.sendKeys("standard_user");// escrever na caixa (colar o texto)
+       // username.sendKeys(Keys.chord("standard_user"));//escreve na caixa letra por letra
+
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button")).click();
+
+        //Transição de página / carregamento de página (lentidão)
+
+        //Verificar se estamos na página interna( Se conseguimos entrar)
+        //Verifica a palavra "Products" em determinado elemento
+        assertEquals(driver.findElement(By.cssSelector("span.title")).getText(),"Products");
+        assertTrue(driver.findElement(By.id("shopping_cart_container")).isDisplayed());
+        // Selecionar o produto que seria o id nº 4
+        driver.findElement(By.id("item_4_title_link")).click();
+        // Transição de tela para a página do produto
+
+        // Validar o nome e o valor
+        assertEquals(driver.findElement(By.cssSelector("div.inventory_details_name.large_size")).getText(),
+                "Sauce Labs Backpack");
+        assertEquals(driver.findElement(By.cssSelector("div.inventory_details_price")).getText(),
+                "$29.99");
+
+        // Clicar no botão Adicionar no Carrinho
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+
+
+        // Clica no icone do Carrinho
+        driver.findElement(By.id("shopping_cart_container")).click();
+        // Outra transição/carregamento de página
+
+        // Verificar o Titulo da Página, nome do produto, quantidade e preço
+        assertEquals(driver.findElement(By.cssSelector("span.title")).getText(), "Your Cart");
+        assertEquals(driver.findElement(By.id("item_4_title_link")).getText(), "Sauce Labs Backpack");
+        assertEquals(driver.findElement(By.cssSelector("div.cart_quantity")).getText(), "1");
+        assertEquals(driver.findElement(By.cssSelector("div.inventory_item_price")).getText(), "$29.99");
+
+    }
 }
